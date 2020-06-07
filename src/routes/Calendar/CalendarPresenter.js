b6c7c852f95d764451bd8a928dd294ee0a9911d5
Hyerim Lee <hermioneee2@gmail.com>
2020-06-07 18:40:38 +0900

    calendar working on

--- src/routes/Calendar/CalendarPresenter.js
+++ src/routes/Calendar/CalendarPresenter.js
@@ -1,17 +1,102 @@
 import { Col, Row } from "antd";
 import React from "react";
 import styled from "styled-components";
+import { Calendar, Badge } from 'antd';
+import MenuCard from "../../components/MenuCard";
+
+
+function getListData(value) {
+  let listData;
+  switch (value.date()) {
+    case 8:
+      listData = [
+        { type: 'warning', content: 'This is warning event.' },
+        { type: 'success', content: 'This is usual event.' },
+      ];
+      break;
+    case 10:
+      listData = [
+        { type: 'warning', content: 'This is warning event.' },
+        { type: 'success', content: 'This is usual event.' },
+        { type: 'error', content: 'This is error event.' },
+      ];
+      break;
+    case 15:
+      listData = [
+        { type: 'warning', content: 'This is warning event' },
+        { type: 'success', content: 'This is very long usual event。。....' },
+        { type: 'error', content: 'This is error event 1.' },
+        { type: 'error', content: 'This is error event 2.' },
+        { type: 'error', content: 'This is error event 3.' },
+        { type: 'error', content: 'This is error event 4.' },
+      ];
+      break;
+    default:
+  }
+  return listData || [];
+}
+
+function dateCellRender(value) {
+  const listData = getListData(value);
+  return (
+    <ul className="events">
+      {listData.map(item => (
+        <li key={item.content}>
+          <Badge status={item.type} text={item.content} />
+        </li>
+      ))}
+    </ul>
+    // <Row gutter={[20, 20]}>
+    //   {group.menus.map((menu) => (
+    //     <Col xs={24} sm={24} md={24} lg={12} xl={8} key={menu.menu.id}>
+    //       <MenuCard menu={menu.menu} />
+    //     </Col>
+    //   ))}
+    // </Row>
+  );
+}
+
+function getMonthData(value) {
+  if (value.month() === 8) {
+    return 1394;
+  }
+}
+
+function monthCellRender(value) {
+  const num = getMonthData(value);
+  return num ? (
+    <div className="notes-month">
+      <section>{num}</section>
+      <span>Backlog number</span>
+    </div>
+  ) : null;
+}
+
 
 const CalendarPresenter = ({ group }) => {
   return (
     <Body>
       <TitleContainer>
         <TitleWrapper>
-          <Title>History Calendar</Title>
+          <Title>Menu History Calendar</Title>
         </TitleWrapper>
       </TitleContainer>
-      <Row>
-        <Col>Test</Col>
+      <Row gutter={30} style={{ paddingTop: 20 }}>
+        <CalendarContainer>
+          <Calendar dateCellRender={dateCellRender} monthCellRender={monthCellRender} />        
+        </CalendarContainer>
+        <Col span={7}>
+          <VoteContainer>
+            <VoteTitle>Recommended Menus</VoteTitle>
+            <Row gutter={[20, 20]}>
+              {group.menus.map((menu) => (
+                <Col xs={24} sm={24} md={24} lg={12} xl={8} key={menu.menu.id}>
+                  <MenuCard menu={menu.menu} />
+                </Col>
+              ))}
+            </Row>
+          </VoteContainer>
+        </Col>
       </Row>
     </Body>
   );
@@ -40,6 +125,26 @@ const Title = styled.h2`
   font-weight: 500;
   color: #13c2c2;
   margin: 0;
+`;
+
+const CalendarContainer = styled.div`
+  width: 800px;
+  padding: 0px 20px;
+`;
+
+const VoteContainer = styled.div`
+  width: 100%;
+  background-color: white;
+  border-radius: 10px;
+  padding: 20px 25px;
+`;
+
+const VoteTitle = styled.h3`
+  font-size: 22px;
+  font-weight: 300;
+  color: rgba(0, 0, 0, 0.65);
+  padding-bottom: 7px;
+  border-bottom: 2px solid rgba(0, 109, 117, 0.2);
 `;
 
 export default CalendarPresenter;
