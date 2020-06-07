0d1db3a4504c856ba937775b2f304483e72e7838
Hyerim Lee <hermioneee2@gmail.com>
2020-06-08 01:35:56 +0900

    Calendar done

--- src/routes/Calendar/CalendarPresenter.js
+++ src/routes/Calendar/CalendarPresenter.js
@@ -5,75 +5,39 @@ import { Calendar, Badge } from 'antd';
 import MenuCard from "../../components/MenuCard";
 
 
-function getListData(value) {
-  let listData;
-  switch (value.date()) {
-    case 8:
-      listData = [
-        { type: 'warning', content: 'This is warning event.' },
-        { type: 'success', content: 'This is usual event.' },
-      ];
-      break;
-    case 10:
-      listData = [
-        { type: 'warning', content: 'This is warning event.' },
-        { type: 'success', content: 'This is usual event.' },
-        { type: 'error', content: 'This is error event.' },
-      ];
-      break;
-    case 15:
-      listData = [
-        { type: 'warning', content: 'This is warning event' },
-        { type: 'success', content: 'This is very long usual event。。....' },
-        { type: 'error', content: 'This is error event 1.' },
-        { type: 'error', content: 'This is error event 2.' },
-        { type: 'error', content: 'This is error event 3.' },
-        { type: 'error', content: 'This is error event 4.' },
-      ];
-      break;
-    default:
-  }
-  return listData || [];
-}
-
-function dateCellRender(value) {
-  const listData = getListData(value);
-  return (
-    <ul className="events">
-      {listData.map(item => (
-        <li key={item.content}>
-          <Badge status={item.type} text={item.content} />
-        </li>
-      ))}
-    </ul>
-    // <Row gutter={[20, 20]}>
-    //   {group.menus.map((menu) => (
-    //     <Col xs={24} sm={24} md={24} lg={12} xl={8} key={menu.menu.id}>
-    //       <MenuCard menu={menu.menu} />
-    //     </Col>
-    //   ))}
-    // </Row>
-  );
-}
-
-function getMonthData(value) {
-  if (value.month() === 8) {
-    return 1394;
-  }
-}
-
-function monthCellRender(value) {
-  const num = getMonthData(value);
-  return num ? (
-    <div className="notes-month">
-      <section>{num}</section>
-      <span>Backlog number</span>
-    </div>
-  ) : null;
-}
-
-
 const CalendarPresenter = ({ group }) => {
+
+  function dateCellRender(value) {
+    if (value.date() == 8 || value.date() == 18)
+    return (
+      <Row>  
+        {group.menus.map((menu) => (
+          <Col xs={24} sm={24} md={24} lg={24} xl={24} key={menu.menu.id} >
+            <MenuContainer>
+              <MenuCard menu={menu.menu} />
+            </MenuContainer>
+          </Col>
+        ))}
+      </Row>
+    );
+  }
+
+  function getMonthData(value) {
+    if (value.month() === 8) {
+      return 1394;
+    }
+  }
+
+  function monthCellRender(value) {
+    const num = getMonthData(value);
+    return num ? (
+      <div className="notes-month">
+        <section>{num}</section>
+        <span>Backlog number</span>
+      </div>
+    ) : null;
+  }
+  
   return (
     <Body>
       <TitleContainer>
@@ -83,11 +47,12 @@ const CalendarPresenter = ({ group }) => {
       </TitleContainer>
       <Row gutter={30} style={{ paddingTop: 20 }}>
         <CalendarContainer>
-          <Calendar dateCellRender={dateCellRender} monthCellRender={monthCellRender} />        
+          <Calendar dateCellRender={dateCellRender} monthCellRender={monthCellRender}/>        
         </CalendarContainer>
         <Col span={7}>
-          <VoteContainer>
-            <VoteTitle>Recommended Menus</VoteTitle>
+          <RecomContainer>
+            <RecomTitle>Recommended Menus</RecomTitle>
+            <Description>What you haven’t eaten for 3 weeks</Description>
             <Row gutter={[20, 20]}>
               {group.menus.map((menu) => (
                 <Col xs={24} sm={24} md={24} lg={12} xl={8} key={menu.menu.id}>
@@ -95,7 +60,7 @@ const CalendarPresenter = ({ group }) => {
                 </Col>
               ))}
             </Row>
-          </VoteContainer>
+          </RecomContainer>
         </Col>
       </Row>
     </Body>
@@ -132,19 +97,32 @@ const CalendarContainer = styled.div`
   padding: 0px 20px;
 `;
 
-const VoteContainer = styled.div`
+const RecomContainer = styled.div`
   width: 100%;
   background-color: white;
   border-radius: 10px;
   padding: 20px 25px;
 `;
 
-const VoteTitle = styled.h3`
+const RecomTitle = styled.h3`
   font-size: 22px;
   font-weight: 300;
   color: rgba(0, 0, 0, 0.65);
   padding-bottom: 7px;
   border-bottom: 2px solid rgba(0, 109, 117, 0.2);
+`;
+
+const Description = styled.p`
+  padding-bottom: 0;
+  font-size: 15px;
+  color: rgba(0, 0, 0, 0.45);
+`;
+
+const MenuContainer = styled.div`
+  width: 50px;
+  align-items: center;
+  padding-bottom: 7px;
+
 `;
 
 export default CalendarPresenter;
