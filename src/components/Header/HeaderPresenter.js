1048c5522acf6a4e83969988cc2e25f4c419d330
Seungsu Kim <seungsu0407@gmail.com>
2020-06-16 01:29:14 +0900

    :sparkles: Add openedAt

--- src/components/Header/HeaderPresenter.js
+++ src/components/Header/HeaderPresenter.js
@@ -1,11 +1,54 @@
-import { StarFilled, UserOutlined } from "@ant-design/icons";
-import { Checkbox } from "antd";
+import { FieldTimeOutlined, StarFilled, UserOutlined } from "@ant-design/icons";
+import { Button, Checkbox, TimePicker } from "antd";
+import Modal from "antd/lib/modal/Modal";
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-const HeaderPresenter = ({ numUser, isJoining, handleJoin }) => (
+const HeaderPresenter = ({
+  numUser,
+  isJoining,
+  handleJoin,
+  openedAt,
+  setOpenedAt,
+  handleOpenedAt,
+  showSetTime,
+  setShowSetTime,
+}) => (
   <Header>
+    <Modal
+      visible={showSetTime}
+      centered
+      footer={null}
+      width={600}
+      onCancel={() => setShowSetTime(false)}
+    >
+      <ModalWrapper>
+        <ModalTitle>Vote Opening Time</ModalTitle>
+        <ModalDescription>
+          The vote is only avaliable until this time.
+          <br />
+          After this time, the vote result will be shown.
+        </ModalDescription>
+        <TimePicker
+          use12Hours
+          minuteStep={5}
+          format="h:mm A"
+          size="large"
+          style={{ width: 200 }}
+          value={openedAt}
+          onChange={(time) => setOpenedAt(time)}
+        />
+        <Button
+          type="primary"
+          size="large"
+          style={{ marginTop: 40, paddingLeft: 40, paddingRight: 40 }}
+          onClick={handleOpenedAt}
+        >
+          Set
+        </Button>
+      </ModalWrapper>
+    </Modal>
     <GroupWrapper>
       <StarFilled style={{ fontSize: 35, color: "#13C2C2" }} />
       <Link to="/">
@@ -15,7 +58,15 @@ const HeaderPresenter = ({ numUser, isJoining, handleJoin }) => (
       <GroupSize>{numUser}</GroupSize>
     </GroupWrapper>
     <HeaderButtonWrapper>
+      <Button
+        size="large"
+        style={{ marginRight: 20 }}
+        onClick={() => setShowSetTime(true)}
+      >
+        <FieldTimeOutlined /> Set Vote Opening Time
+      </Button>
       <CheckboxButton
+        size="large"
         checked={!isJoining}
         onClick={() => handleJoin(!isJoining)}
       >
@@ -63,6 +114,26 @@ const CheckboxButton = styled(Checkbox)`
   padding: 5px 10px;
   border: 1px solid #13c2c2;
   border-radius: 5px;
+`;
+
+const ModalWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 40px 30px;
+`;
+
+const ModalTitle = styled.h3`
+  color: rgba(0, 0, 0, 0.65);
+  font-weight: 900;
+  font-size: 30px;
+  text-align: center;
+`;
+
+const ModalDescription = styled.p`
+  color: rgba(0, 0, 0, 0.65);
+  font-size: 20px;
+  text-align: center;
 `;
 
 export default HeaderPresenter;
